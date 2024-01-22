304ab2f0a2e1be3b3f6cf06f17e5e0312b6dcac1
rakshanayak01 <rakshanayak01@gmail.com>
2024-01-22 16:47:33 +0530

    copy

---
+++ git/src/git/copy.java
@@ -0,0 +1,16 @@
+package git;
+
+public class copy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+		System.out.println("a");
+		System.out.println("b");
+	}
+
+}
