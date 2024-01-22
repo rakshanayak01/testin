c1648796b56b5020bca630d2122e5a61a9e0c41c
rakshanayak01 <rakshanayak01@gmail.com>
2024-01-22 16:32:15 +0530

    first

---
+++ git/src/git/code1.java
@@ -0,0 +1,13 @@
+package git;
+
+public class code1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+	}
+
+}
